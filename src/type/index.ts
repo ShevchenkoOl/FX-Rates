12f08b4d4d1bea4619134aa94fbcc1f5938dc1ae
Oleksii Shevchenko <105150555+ShevchenkoOl@users.noreply.github.com>
2026-02-02 00:42:37 +0100

    updated index,ts
    Removed commented-out code for CacheData example.

--- src/type/index.ts
+++ src/type/index.ts
@@ -6,10 +6,3 @@ export interface CacheData {
     rates: CurrentyRate[],
     lastUpdated: Date | null;
 };
-
-// const update: CacheData = {
-//     rates: [{usd: 40}, {eur: 50}],
-//     lastUpdate: null
-// };
-
-// console.log(update);
